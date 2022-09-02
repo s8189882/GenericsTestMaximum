d7973b11346df3232e3314e3acfcdc7da51fb51f
s8189882 <s8189882@gmail.com>
2022-09-02 17:17:19 +0530

    Add UC1 - Given 3 Integers find the maximum and test cases

--- Generics Find Maximum Element/src/generics/max/element/test/MaximumElement.java
+++ Generics Find Maximum Element/src/generics/max/element/test/MaximumElement.java
@@ -4,5 +4,27 @@ public class MaximumElement {
 	
 	public static void main(String[] args) {
 		System.out.println("Test Maximum of 3 variables\n");
+		
+		Integer firstNum = 10;
+		Integer secondNum = 20;
+		Integer thirdNum = 30;
+		
+		findMax(firstNum, secondNum, thirdNum);
+	}
+	
+	public static void findMax(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+		Integer maximumNumber;
+		
+		if(firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0) {
+			maximumNumber = firstNumber;
+		}
+		else if(secondNumber.compareTo(firstNumber)>0 && secondNumber.compareTo(thirdNumber)>0) {
+			maximumNumber = secondNumber;
+		}
+		else {
+			maximumNumber = thirdNumber;
+		}
+		
+		System.out.println(maximumNumber + " is maximum out of "+firstNumber+", "+secondNumber+" and "+thirdNumber);
 	}
 }
